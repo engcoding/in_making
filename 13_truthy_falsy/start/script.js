83c9362dcdc2a513a901f44600394fb79ec2dacc
engcoding <engcoding8872@gmail.com>
2022-01-12 13:13:23 +0900

    13_truthy_falsy

---
+++ 13_truthy_falsy/start/script.js
@@ -0,0 +1,130 @@
+// 13. Truthy, Falsy
+// Boolean()
+// 0은 falsy
+
+// console.log(Boolean(0));
+// console.log(typeof Boolean(0));
+//
+// // empty string
+// console.log(Boolean(""));
+// console.log(typeof Boolean(""));
+
+// null, undefined, NaN
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+//
+// // empty object {}
+// console.log(Boolean({}));
+//
+// const girlFriend = 1;
+//
+// if (girlFriend) {
+//     console.log("Date with her.");
+// } else {
+//     console.log("Learn coding.");
+// }
+
+// undefined
+// let myVariable;
+let myVariable = "something";
+
+if (myVariable) {
+    console.log("myVariable is defined correctly.");
+} else {
+    console.log("myVariable is undefined");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
