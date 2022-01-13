8a77f9e719c8a5703cd8177ea20f7cdc81cb7aba
engcoding <engcoding8872@gmail.com>
2022-01-13 13:08:40 +0900

    19_expression_statement

--- 18_expression_vs_statement/script.js
+++ 18_expression_vs_statement/script.js
@@ -1,29 +1,19 @@
 // 18. expression, statement
-const userName = "Song";
-
+// expression(표현식), statement(선언문)
+// if - else
 const age = 18;
 if (age >= 18) {
     console.log("You can vote.");
 } else {
-    console.log("You have to wait");
+    console.log("You have to wait.");
 }
 
-// exrpession vs statement
+// `string text ${expression} string text`;
 const currentYear = 2022;
 const birthYear = 2000;
-// const userAge = `Your age is ${currentYear - birthYear}.`;
-const userAge = currentYear - birthYear;
-const userAge = `Your age is ${if(userAge > 18) console.log("You can vote")}.`;
-
-
-
-
-
-
-
-
-
-
+// const userAge = `Your age is ${currentYear - birthYear}`;
+const userAge = `Your age is ${if (yourAge > 18) { console.log("You can vote")}}`;
+console.log(userAge);
 
 
 
