1ce4ef5f125e20df1cb20425e21360743d8cd467
engcoding <engcoding8872@gmail.com>
2022-01-13 13:10:52 +0900

    19_expression_statement_completed

---
+++ 18_expression_vs_statement/final/script.js
@@ -0,0 +1,29 @@
+// 18. expression, statement
+// expression(표현식), statement(선언문)
+// if - else
+const age = 18;
+if (age >= 18) {
+    console.log("You can vote.");
+} else {
+    console.log("You have to wait.");
+}
+
+// `string text ${expression} string text`;
+const currentYear = 2022;
+const birthYear = 2000;
+// const userAge = `Your age is ${currentYear - birthYear}`;
+const userAge = `Your age is ${if (yourAge > 18) { console.log("You can vote")}}`;
+console.log(userAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
